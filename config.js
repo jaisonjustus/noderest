7345afec24bc9db15e9ca199718f2d741f2b01cd
jaisonjustus <jaison.justus.lp@gmail.com>
2012-11-26 17:35:34 +0530

    removed old folders

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	mongo : {
-		connectionString : 'mongodb://nodejitsu:dee9f440c8bd0cf4946b08af5de34e93@alex.mongohq.com:10018/collection-exchange',
-		dbName : 'collection-exchange'
-	}
-};
